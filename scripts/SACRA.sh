1e65bc21bf8cf39f627dfdf1996941d63ebc854e
Wataru <wataru.suda@riken.jp>
2019-10-24 15:44:06 +0900

    Add multithread options

--- scripts/SACRA.sh
+++ scripts/SACRA.sh
@@ -4,23 +4,60 @@
 
 fasta=$1        # fasta file
 prefix=$2       # prefix
-th=50		# No. of threads for LAST aligner
+th=50		    # No. of threads for LAST and multithreads
 
+echo -e "***** [$0] start " `date +'%Y/%m/%d %H:%M:%S'` " *****\n"
+
+# Perform LAST alignment
+echo -e "lastdb -P $th -R01 -uNEAR $fasta $fasta"
 lastdb -P $th -R01 -uNEAR $fasta $fasta
+echo -e "lastal -a 8 -A 16 -b 12 -B 5 -S 1 -P $th -f BlastTab+ $fasta $fasta > $fasta.blasttab"
 lastal -a 8 -A 16 -b 12 -B 5 -S 1 -P $th -f BlastTab+ $fasta $fasta > $fasta.blasttab
+echo -e "DONE"
 
+# PARs
+echo -e "PARs detection"
 perl SACRA_PARs_depth.pl -i $fasta.blasttab -al 100 -tl 50 -pd 5 -id 75 > $fasta.blasttab.depth
-perl SACRA_PCratio.pl -i $fasta.blasttab -pa $fasta.blasttab.depth -ad 50 -id 75 > $fasta.blasttab.depth.pcratio
+echo -e "DONE"
+
+# PC ratio
+echo -e "PC ratio calculation"
+perl SACRA_multi.pl $th $fasta.blasttab.depth
+for i in `ls  *.depth.split*`;do perl SACRA_PCratio.pl -i $fasta.blasttab -pa $i -ad 50 -id 75 > $i.pcratio & done
+wait
+cat $fasta*.depth.split*.pcratio > $fasta.blasttab.depth.pcratio
+rm -rf $fasta*.depth.split*
+echo -e "DONE"
+
+# Split reads
+echo -e "Split reads"
 perl SACRA_split.pl -i $fasta.blasttab.depth.pcratio -pc 15 -dp 10 -sl 100 > $fasta.blasttab.depth.pcratio.faidx
+perl SACRA_multi.pl $th $fasta.blasttab.depth.pcratio.faidx
+
+for i in `ls $fasta.blasttab.depth.pcratio.faidx.split*`
+do
+    for j in `less $i`
+    do
+        echo "samtools faidx $fasta $j >> $i.fasta" >> $i.sh
+    done
+done
+
+samtools faidx $fasta
+chmod +x $fasta.blasttab.depth.pcratio.faidx.split*sh
+for i in `ls $fasta.blasttab.depth.pcratio.faidx.split*sh`
+do
+    bash $i &
+done
+wait
+cat $fasta.blasttab.depth.pcratio.faidx.split*fasta > $fasta.split.fasta
+rm -rf $fasta.blasttab.depth.pcratio.faidx.split*
+echo -e "DONE"
+
+# Output reads
+echo -e "Output final reads"
 awk -F ":" '{print $1}' $fasta.blasttab.depth.pcratio.faidx | uniq > $fasta.blasttab.depth.pcratio.faidx.id
 seqkit grep -v -f $fasta.blasttab.depth.pcratio.faidx.id $fasta > $fasta.non_chimera.fasta
 
-for i in `less $fasta.blasttab.depth.pcratio.faidx`
-do
-echo "samtools faidx $fasta $i >> $fasta.split.fasta" >> $fasta.faidx.sh
-done
-
-chmod +x $fasta.faidx.sh
-bash $fasta.faidx.sh
-
 cat $fasta.non_chimera.fasta $fasta.split.fasta > $prefix
+
+echo "***** [$0] end " `date +'%Y/%m/%d %H:%M:%S'` " *****"
